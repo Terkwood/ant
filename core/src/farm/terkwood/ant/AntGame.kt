0376aa6cf80b9ea27176ab7b74b78ba9ba2096bd
Terkwood <metaterkhorn@gmail.com>
2019-11-11 18:12:23 -0500

    Update AntGame.kt

--- core/src/farm/terkwood/ant/AntGame.kt
+++ core/src/farm/terkwood/ant/AntGame.kt
@@ -2,11 +2,14 @@ package farm.terkwood.ant
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Animation
+import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 import ktx.collections.gdxArrayOf
@@ -56,8 +59,13 @@ class AntGame : ApplicationAdapter() {
     }
 
     override fun render() {
+        val delta = Gdx.graphics.deltaTime
         Gdx.gl.glClearColor(0.2f, 0.5f, 0.8f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        stage?.act(delta)
+        stage?.draw()
+
         batch!!.begin()
         batch!!.draw(img, 0f, 0f)
         batch!!.end()
@@ -66,5 +74,21 @@ class AntGame : ApplicationAdapter() {
     override fun dispose() {
         batch!!.dispose()
         img!!.dispose()
+        stage!!.dispose()
+    }
+}
+
+class AntActor : Actor() {
+    private val region = TextureRegion()
+
+    init {
+        setBounds(region.regionX.toFloat(), region.regionY.toFloat(),
+            region.regionWidth.toFloat(), region.regionHeight.toFloat())
+    }
+
+    override fun draw(batch: Batch?, parentAlpha: Float) {
+        val color = color
+        batch?.color = Color(color.r, color.g, color.b, color.a * parentAlpha)
+        batch?.draw(region, x, y, originX, originY, width, height, scaleX, scaleY, rotation)
     }
 }
